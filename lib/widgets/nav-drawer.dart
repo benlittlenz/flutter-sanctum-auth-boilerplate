5c41316567f3d688551d18e4e14aa3ec89575778
benlittlenz <benlittle53@gmail.com>
2021-04-05 11:54:50 +1200

    created nav drawer and posts screen

---
+++ lib/widgets/nav-drawer.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sanctum_boilerplate/screens/posts-screen.dart';
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          ListTile(
+            title: Text('User Name'),
+          ),
+          ListTile(
+            title: Text('Posts'),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) => PostScreen()))
+            }
+          ),
+        ],
+      ),
+    );
+  }
+}
